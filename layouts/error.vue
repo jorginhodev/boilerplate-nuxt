6513f90658bded9fa1f2ea93cf870b4551a84fbe
Jorge Ramos <jorginhodeveloper@gmail.com>
2021-01-03 16:09:16 -0300

    first commit

---
+++ layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 v-if="error.statusCode === 404">Página não encontrada</h1>
+    <h1 v-else>Ocorreu um erro</h1>
+    <NuxtLink to="/">Página Inicial</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default() {
+        return []
+      },
+    },
+  },
+  layout: 'error',
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <main class="container">
+    <h1 class="title">Boilerplate NuxtJS</h1>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
